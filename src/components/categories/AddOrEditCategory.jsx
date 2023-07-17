dfc888d1cfe549f200c73157c6da9094a95e1f2b
khanhnd52 <khanh.nd5252@gmail.com>
2023-07-17 16:21:52 +0700

    add insert category functions

--- src/components/categories/AddOrEditCategory.jsx
+++ src/components/categories/AddOrEditCategory.jsx
@@ -1,27 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import withRouter from '../../helpers/withRouter';
-import { PageHeader } from '@ant-design/pro-components';
-import { Button, Col, Divider, Form, Input, Row, Select } from 'antd';
-import ContentHeader from '../common/ContentHeader';
+import withRouter from "../../helpers/withRouter";
+import {insertCategory} from "../../redux/actions/categoryAction"
+import { Button, Col, Divider, Form, Input, Row, Select } from "antd";
+import ContentHeader from "../common/ContentHeader";
+import { connect } from "react-redux";
 
 class AddOrEditCategory extends Component {
-  onSubmitForm = (values) => { console.log(values) }
+  onSubmitForm = (values) => {
+    console.log(values);
+    const { navigate } = this.props.router;
+    this.props.insertCategory(values, navigate);
+  };
   render() {
-    const {navigate} = this.props.router;
+    const { navigate } = this.props.router;
     return (
       <div>
-        <ContentHeader navigate={navigate} title="Add New Category" className="site-page-header"></ContentHeader>
+        <ContentHeader
+          navigate={navigate}
+          title="Add New Category"
+          className="site-page-header"
+        ></ContentHeader>
 
-        <Form layout='vertical' className='form' onFinish={this.onSubmitForm}>
+        <Form layout="vertical" className="form" onFinish={this.onSubmitForm}>
           <Row>
-            <Col md={12} >
+            <Col md={12}>
               <Form.Item label="Category ID" name="categoryId">
                 <Input readOnly></Input>
               </Form.Item>
-              <Form.Item label="Name" name="name" rules={[
-                {required: true, min: 2, }
-              ]}>
+              <Form.Item
+                label="Name"
+                name="name"
+                rules={[{ required: true, min: 2 }]}
+              >
                 <Input></Input>
               </Form.Item>
               <Form.Item label="Stauts" name="status" initialValue={"0"}>
@@ -31,13 +42,30 @@ class AddOrEditCategory extends Component {
                 </Select>
               </Form.Item>
               <Divider></Divider>
-              <Button htmlType='submit' type='primary' style={{float: 'right'}}>Save</Button>
+              <Button
+                htmlType="submit"
+                type="primary"
+                style={{ float: "right" }}
+              >
+                Save
+              </Button>
             </Col>
           </Row>
         </Form>
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(AddOrEditCategory);
+
+
+
+const mapStateToProps = (state) => ({
+  category: state.categoryReducer.category
+})
+
+const mapDispatchToProps = {
+  insertCategory
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AddOrEditCategory))
